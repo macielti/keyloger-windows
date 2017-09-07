32831f036a002b3d4782e94c650cc6c186a11067
Bruno do Nascimento Macie <brunodonascimentomaciel@gmail.com>
2017-09-06 22:00:05 -0300

    salvando progresso

--- Analisador de logs/main.py
+++ Analisador de logs/main.py
@@ -1,6 +1,7 @@
 import os
 import gmail
 import facebook
+import sigaa
 
 linhas=[]
 
@@ -17,37 +18,91 @@ def set_up():
 
 def analisar_log(arquivo):
     
+    lines=[]
+    
     arq= open(arquivo, 'r', encoding = "ISO-8859-1")
     texto= arq.readlines()
     for linha in texto:
-        linhas.append(linha)
+        lines.append(linha)
     arq.close()
+    
+    return lines
 
-def pesq_arq_analis():
-
-    for arquivo in os.listdir('analisar'):
-        print('\t'+arquivo)
         
 ################################################################################
+#Esta lista recebe as linhas dos logs, cada elemento é uma lista
+lista_composta=[]
+
+#esta lista deve armazenar o nome dos arquivos de logs analisados para, que os
+#arquivos de saida possam ser nomeados corretamente
+ordem_dos_arq=[]
+
+#loop para ser executado em cima de cada arquivo encontrado na pasta de
+#analise
+for arquivo in os.listdir('analisar'):
+    
+    #armazeno uma lista com todas as linhas do arquivo no ciclo atual em uma
+    #variável
+    lines_arquivo= analisar_log(arquivo)
+    
+    #adiciono a outra lista a lista que foi gerada na linha anterior, assim
+    #teremos um elemento-lista para cada arquivo, armazenado na lista_composta
+    lista_composta.append(lines_arquivo)
+    
+    #Adiciono o nome do arquivos na ordem que foi analisado, o nome do arquivo
+    #está sem o .txt 
+    ordem_dos_arq.appdend(arquivo[:-4])
+
+#list_lines_log recebe cada elemento da lista composta, que é uma outra lista
+#com as linhas de cada arquivona pasta de analise
+for list_lines_log in lista_composta:
+    for 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+os.system('clear')
 
 #Verificar e criar as pastas necessárias para rodar o programa
 set_up()
 
 opc=input('Que tipo de conta você deseja pesquisar?\n\n\t1 para Facebook\n\t\
-2 para Gmail\n')
+2 para Gmail\n\t3 para SIGAA')
 
 os.system('clear')
 
-print('Lista de arquivos a serem analisados:\n')
-pesq_arq_analis()
-arquivo=input('Digite o nome do arquivo sem o \'.txt\':')
+logs= os.listdir('analisar')
 
-os.system('clear')
+for log in logs:
+    
+    analisar_log('analisar/'+log)
+    
+    if opc =='1':
+        facebook.facebook(linhas, logs)
 
-analisar_log('analisar/'+arquivo+'.txt')
+    if opc=='2':
+        gmail.gmail(linhas, logs)
 
-if opc =='1':
-    facebook.facebook(linhas, arquivo)
-
-if opc=='2':
-    gmail.gmail(linhas, arquivo)
+    if opc=='3':
+        sigaa.sigaa(linhas, logs)
---
+++ Analisador de logs/sigaa.py
@@ -0,0 +1,23 @@
+import os
+
+def sigaa(lista_de_linhas, arquivo):
+    escrever=[]
+    contador=0
+    for linha in lista_de_linhas:
+        ocorrencia= linha.find('SIGAA')
+
+        
+        if ocorrencia != -1:
+            escrever.append(linha)
+            
+            second_count=contador
+            while lista_de_linhas[second_count].find('Window') == -1:
+                if len(lista_de_linhas[second_count]) ==0:
+                    break
+                escrever.append(lista_de_linhas[second_count])
+                second_count+=1
+        
+        contador +=1
+    
+    for l in escrever:
+        os.system('echo " '+l+' " >> saida/'+arquivo+'__sigaa.txt')
